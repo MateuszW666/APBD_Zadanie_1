fe482e05ac6cc86f0a6eecbf6ffa34e644fe2919
s22976 <s22976@pjwstk.edu.pl>
2024-03-19 18:58:30 +0100

    Requirement implementation

--- Inicial Project/Program.cs
+++ Inicial Project/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace AverageCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World z Modyfikacją3!");
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            double average = CalculateAverage(numbers);
+            Console.WriteLine($"Średnia: {average}");
+        }
+
+        static double CalculateAverage(int[] numbers)
+        {
+            if (numbers.Length == 0) return 0;
+            double sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum / numbers.Length;
         }
     }
 }
+
